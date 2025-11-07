f4a42529a86c0ff958f30b75f341e4f4b2e1e61c
daumantasp <daumantasp@gmail.com>
2025-11-07 15:08:09 +0200

    Add GetPowerPlantsHandler ues case.

---
+++ src/IgnitisPowerPlants.Application/UseCases/GetPowerPlantsHandler.cs
@@ -0,0 +1,22 @@
+﻿using IgnitisPowerPlants.Application.Interfaces;
+using IgnitisPowerPlants.Application.Responses;
+
+namespace IgnitisPowerPlants.Application.UseCases
+{
+    internal class GetPowerPlantsHandler
+    {
+        private readonly IPowerPlantsRepository _powerPlantsRepository;
+        public GetPowerPlantsHandler(IPowerPlantsRepository powerPlantsRepository)
+        {
+            _powerPlantsRepository = powerPlantsRepository;
+        }
+
+        public async Task<PowerPlantsResponse> HandleAsync()
+        {
+            var powerPlants = await _powerPlantsRepository.GetAsync();
+            var powerPlantsDtos = powerPlants.Select(pp => pp.ToDto()).ToList();
+
+            return new PowerPlantsResponse { PowerPlants = powerPlantsDtos };
+        }
+    }
+}
