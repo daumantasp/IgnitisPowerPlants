3b093a51c2fd062f99efa282a92184eae925f1fc
daumantasp <daumantasp@gmail.com>
2025-11-07 14:29:37 +0200

    Add IPowerPlantsRepository interface.

---
+++ src/IgnitisPowerPlants.Application/Interfaces/IPowerPlantsRepository.cs
@@ -0,0 +1,9 @@
+﻿using IgnitisPowerPlants.Domain.Entities;
+
+namespace IgnitisPowerPlants.Application.Interfaces
+{
+    public interface IPowerPlantsRepository
+    {
+        Task<IEnumerable<PowerPlant>> GetAsync();
+    }
+}
