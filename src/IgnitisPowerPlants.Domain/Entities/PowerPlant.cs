c5c65abb94f406976f3c3938773463b56337859a
daumantasp <daumantasp@gmail.com>
2025-11-07 07:51:36 +0200

    Add PowerPlant entity.

---
+++ src/IgnitisPowerPlants.Domain/Entities/PowerPlant.cs
@@ -0,0 +1,11 @@
+﻿namespace IgnitisPowerPlants.Domain.Entities
+{
+    public class PowerPlant
+    {
+        public int Id { get; set; }
+        public required string Owner { get; set; }
+        public decimal Power { get; set; }
+        public DateOnly ValidFrom { get; set; }
+        public DateOnly? ValidTo { get; set; }
+    }
+}
