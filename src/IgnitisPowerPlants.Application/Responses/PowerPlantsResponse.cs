d27f7b22d9d0c46445b7c184c72e6e5df82fc7a5
daumantasp <daumantasp@gmail.com>
2025-11-07 14:48:23 +0200

    Add PowerPlantsResponse.

---
+++ src/IgnitisPowerPlants.Application/Responses/PowerPlantsResponse.cs
@@ -0,0 +1,9 @@
+﻿using IgnitisPowerPlants.Application.Dtos;
+
+namespace IgnitisPowerPlants.Application.Responses
+{
+    internal class PowerPlantsResponse
+    {
+        public required List<PowerPlantDto> PowerPlants { get; set; }
+    }
+}
