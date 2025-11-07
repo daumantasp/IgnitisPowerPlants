7b58b50a36f4098acc0be9505255398a337b790d
daumantasp <daumantasp@gmail.com>
2025-11-07 14:36:57 +0200

    Implement PowerPlantsRepository.

---
+++ src/IgnitisPowerPlants.Infrastructure/Repositories/PowerPlantsRepository.cs
@@ -0,0 +1,20 @@
+﻿using IgnitisPowerPlants.Application.Interfaces;
+using IgnitisPowerPlants.Domain.Entities;
+using IgnitisPowerPlants.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace IgnitisPowerPlants.Infrastructure.Repositories
+{
+    internal class PowerPlantsRepository : IPowerPlantsRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PowerPlantsRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<PowerPlant>> GetAsync() => 
+            await _dbContext.PowerPlants.ToListAsync();
+    }
+}
