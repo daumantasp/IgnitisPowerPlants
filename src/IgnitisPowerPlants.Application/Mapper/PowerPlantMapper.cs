4a96df201750058fe83b0ecbd25cff58df9461ab
daumantasp <daumantasp@gmail.com>
2025-11-07 14:52:23 +0200

    Add PowerPlantMapper for entity to DTO conversion.

---
+++ src/IgnitisPowerPlants.Application/Mapper/PowerPlantMapper.cs
@@ -0,0 +1,11 @@
+﻿using IgnitisPowerPlants.Application.Dtos;
+using IgnitisPowerPlants.Domain.Entities;
+
+namespace IgnitisPowerPlants.Application.Mapper
+{
+    internal static class PowerPlantMapper
+    {
+        public static PowerPlantDto ToDto(this PowerPlant entity)
+            => new PowerPlantDto(entity.Owner, entity.Power, entity.ValidFrom, entity.ValidTo);
+    }
+}
